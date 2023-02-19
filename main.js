64cfd91411eae341d47b8d9fe3a8f60abf022833
virajghatgeiv <virajghatgeiv@gmail.com>
2023-02-19 17:28:56 +0530

    Modification for use of local storage

--- main.js
+++ main.js
@@ -1,16 +1,36 @@
 window.addEventListener("load",() =>{
+
+  todos =JSON.parse(localStorage.getItem("todos")) || [];
  const form = document.querySelector("#new-task-form");
  const input = document.querySelector("#new-task-input");
- const list_el = document.querySelector("#tasks");
 
- form.addEventListener("submit",(e) =>{
+ form.addEventListener("submit",e =>{
   e.preventDefault();
-  const task = input.value;
+  
+  if(!e.target.elements.content.value){
+    alert("Please enter some task it can't be blank !");
+    return;
+  }else{
+    const task ={
+        content: e.target.elements.content.value,
+        createdAt: new Date().getTime()
+      }
 
-  if(!task){
-   alert("Please add some task");
-   return;
-  }
+      todos.push(task);
+      localStorage.setItem("todos",JSON.stringify(todos));
+      e.target.reset();
+      DisplayTodos();
+     }
+   });
+    DisplayTodos();
+});
+
+function DisplayTodos(){
+  const list_el = document.querySelector("#tasks");
+
+  list_el.innerHTML = "";
+
+  todos.forEach(todo => {
   const task_el =document.createElement("div");
   task_el.classList.add("task");
 
@@ -20,8 +40,9 @@ window.addEventListener("load",() =>{
   const task_input_el = document.createElement("input");
   task_input_el.classList.add("text");
   task_input_el.type = "text";
-  task_input_el.value = task;
+  task_input_el.value = todo.content ;
   task_input_el.setAttribute("readonly","readonly");
+
 
   const task_actions_el = document.createElement("div");
   task_actions_el.classList.add("actions");
@@ -40,23 +61,32 @@ window.addEventListener("load",() =>{
   task_el.appendChild(task_content_el);
   task_el.appendChild(task_actions_el);
   list_el.appendChild(task_el);
-  input.value = "";
 
-  task_edit_el.addEventListener("click",() => {
-   if(task_edit_el.innerText.toLowerCase() == "edit"){
-    task_input_el.removeAttribute("readonly");
-    task_input_el.focus();
-    task_edit_el.innerText = "Save";
-   }else{
-    task_input_el.setAttribute("readonly","readonly");
-    task_edit_el.innerText = "Edit";
-   }
-
+  task_edit_el.addEventListener("click", e => {
+   const input = task_content_el.querySelector("input");
+   input.removeAttribute("readonly");
+   input.focus();
+   input.addEventListener("blur",(e) =>{
+    console.log(input.value);
+    if(!input.value){
+      alert("Please enter some task it can't be blank !");
+      return;
+    }else{
+    input.setAttribute("readonly",true);
+    todo.content = e.target.value;
+    localStorage.setItem("todos",JSON.stringify(todos));
+    DisplayTodos();
+    }
+    
+   });
    
   });
 
   task_delete_el.addEventListener("click",() =>{
-    list_el.removeChild(task_el);
+    todos = todos.filter(t => t != todo);
+    localStorage.setItem("todos",JSON.stringify(todos));
+    DisplayTodos();
    });
- });
-});
+
+  });
+}
